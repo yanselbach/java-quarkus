6453afe1672b31b7a97cb396334d9a29a037bec2
Frederik Bülthoff <frederik.buelthoff@capgemini.com>
2022-11-21 22:37:54 +0100

    add resilience/fault tolerance when calling The Bored API
    added Timeout and Fallback support when retrieving a random activity.

--- backend/src/main/java/org/example/app/task/logic/ActivityService.java
+++ backend/src/main/java/org/example/app/task/logic/ActivityService.java
@@ -1,8 +1,12 @@
 package org.example.app.task.logic;
 
+import java.time.temporal.ChronoUnit;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Timeout;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 /**
@@ -20,8 +24,20 @@ public class ActivityService {
    *
    * @return the activity description
    */
+  @Fallback(fallbackMethod = "fallbackActivity")
+  @Timeout(unit = ChronoUnit.SECONDS, value = 5)
   public String getRandomActivity() {
 
     return this.boredApi.getRandomActivity().activity;
+  }
+
+  /**
+   * Fallback activity in case the suggestion service is down.
+   *
+   * @return the activity description
+   */
+  public String fallbackActivity() {
+
+    return "Write more tests";
   }
 }
